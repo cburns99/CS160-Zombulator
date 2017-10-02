6494595836382e2d376413a9a32596ea0e53a51d
Cameron Burns <burnscam@oregonstate.edu>
2017-10-02 09:49:39 -0700

    draw circles

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 // zombulator by Cameron Burns
 
 function setup() {
+	createCanvas(800, 800);
 
 }
 
 function draw() {
-	
+	fill(260, 0, 0);
+	ellipse(50, 50, 80, 80);
+	fill(150, 150, 200);
+	ellipse(200, 100, 80, 80);
 }
