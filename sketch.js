13fc76a5389b31d072e35b4afecedab2095be255
Cameron Burns <burnscam@oregonstate.edu>
2017-10-04 09:45:11 -0700

    Added zombie

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 // zombulator by Cameron Burns
 
 var zombieX = 50;
+var Zombie2X = 100;
 
 function setup() {
 	createCanvas(800, 800);
@@ -11,6 +12,7 @@ function draw() {
 	fill(260, 0, 0);
 	ellipse(zombieX, 50, 80, 80);
 	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80);
+	ellipse(Zombie2X, 100, 80, 80);
 	zombieX = zombieX +1;
+	Zombie2X = Zombie2X +2;
 }
