904f21f0b1b793068b93782a2b20c4c2f92e2ad6
Cameron Burns <burnscam@oregonstate.edu>
2017-10-06 14:22:51 -0700

    Changed whole code to attempt to make zombies wrap around

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
 // zombulator by Cameron Burns
 
-var zombieX = 50;
-var Zombie2X = 100;
+var zombieA = 0;
+var zombieAspeed = -1;
+var zombieAsize = 0;
+var startPosition = 0;
+var endPosition = 800;
 
 function setup() {
 	createCanvas(800, 800);
 }
 
 function draw() {
-	background(255, 255, 255);
-	fill(260, 0, 0);
-	ellipse(zombieX, 50, 80, 80);
-	fill(150, 150, 200);
-	ellipse(Zombie2X, 100, 80, 80);
-	zombieX = zombieX +1;
-	Zombie2X = Zombie2X +2;
+	background(0);
+	if (zombieA >= startPosition && zombieAspeed <= 0) {
+		fill(random(50, 255), random(50, 255), random(50, 255));
+		zombieAsize = random(30, 200);
+		startPosition = zombieAsize / 2;
+	}
+
+	if (zombieAspeed < 0 && zombieA >= 800 - (zombieAsize / 2)) {
+		zombieAspeed = random(3, 6);
+	} else if (zombieAspeed < 0 && zombieA > zombieAsize / 2) {
+		zombieAspeed = random(3, 6);
+		zombieAsize = random(30, 200);
+		fill(random(50, 255), random(50, 255), random(50, 255));
+	}
+
+	ellipse(zombieA, 200, zombieAsize, zombieAsize);
+	zombieA = zombieA + zombieAspeed;
 }
