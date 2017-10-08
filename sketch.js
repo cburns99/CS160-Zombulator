a830d9a02ea594582acbdae7ecf761ef2dcfa1fb
Cameron Burns <burnscam@oregonstate.edu>
2017-10-08 12:35:48 -0700

    tried to fix issues

--- sketch.js
+++ sketch.js
@@ -1,31 +1,34 @@
 // zombulator by Cameron Burns
 
-var zombieA = 0;
-var zombieAspeed = -1;
-var zombieAsize = 0;
+var zombieA = 100;
+var zombieAspeed = 5;
+var zombieAsize;
 var startPosition = 0;
 var endPosition = 800;
+var zombieAcolor;
 
 function setup() {
 	createCanvas(800, 800);
+	zombieAsize = random (20, 300)
+	zombieAcolor = color(random(255), random(255), random(255));
 }
 
 function draw() {
 	background(0);
-	if (zombieA >= startPosition && zombieAspeed <= 0) {
-		fill(random(50, 255), random(50, 255), random(50, 255));
-		zombieAsize = random(30, 200);
-		startPosition = zombieAsize / 2;
-	}
-
-	if (zombieAspeed < 0 && zombieA >= 800 - (zombieAsize / 2)) {
-		zombieAspeed = random(3, 6);
-	} else if (zombieAspeed < 0 && zombieA > zombieAsize / 2) {
-		zombieAspeed = random(3, 6);
-		zombieAsize = random(30, 200);
-		fill(random(50, 255), random(50, 255), random(50, 255));
-	}
-
+	fill(random(50, 255), random(50, 255), random(50, 255))
 	ellipse(zombieA, 200, zombieAsize, zombieAsize);
 	zombieA = zombieA + zombieAspeed;
+
+	if (zombieA <= endPosition && zombieAspeed <= 0) {
+		zombieAspeed = random(3, 6)
+		zombieAcolor(random(50, 255), random(50, 255), random(50, 255));
+		zombieAsize = random(30, 200);
+	}
+
+	if (zombieA >= endPosition && zombieAspeed >= 0) {
+		zombieAspeed = random(-3, -6);
+		zombieAcolor(random(50, 255), random(50, 255), random(50, 255));
+		zombieAsize = random(30, 200);
+	} 
+
 }
