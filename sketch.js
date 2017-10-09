11ae0398c8d6fd9cf76558143285d141c631f65a
Cameron Burns <burnscam@oregonstate.edu>
2017-10-09 11:05:09 -0700

    changed human and zombie to make them bounce

--- sketch.js
+++ sketch.js
@@ -1,34 +1,56 @@
 // zombulator by Cameron Burns
 
-var zombieA = 100;
-var zombieAspeed = 5;
-var zombieAsize;
-var startPosition = 0;
-var endPosition = 800;
-var zombieAcolor;
+var humanY = 100;
+var humanV = 0;
+var humanA = 0.2;
+var humanDamping = -0.7;
+var humanSize = 80;
+var humanColor;
+var backgroundColor;
+var zombieY = 130;
+var zombieV = 0;
+var zombieA = 0.5;
+var zombieSize = 180;
+var zombieDamping = -0.8
+var zombieColor;
 
 function setup() {
-	createCanvas(800, 800);
-	zombieAsize = random (20, 300)
-	zombieAcolor = color(random(255), random(255), random(255));
+	createCanvas(windowWidth, windowHeight);
+	humanColor = color(random(255), random(255), random(255));
+	zombieColor = color(random(255), random(255), random(255));
+	backgroundColor = color(random(255), random(255), random(255));
 }
 
 function draw() {
-	background(0);
-	fill(random(50, 255), random(50, 255), random(50, 255))
-	ellipse(zombieA, 200, zombieAsize, zombieAsize);
-	zombieA = zombieA + zombieAspeed;
+	background(backgroundColor); 
+	fill(humanColor);
+	stroke(200, 140, 20);
+	strokeWeight(10);
+	ellipse(windowWidth / 2, humanY, humanSize, humanSize);
+	humanY += humanV;
+	humanV += humanA;
 
-	if (zombieA <= endPosition && zombieAspeed <= 0) {
-		zombieAspeed = random(-3, -6)
-		zombieAcolor(random(50, 255), random(50, 255), random(50, 255));
-		zombieAsize = random(30, 200);
+	if (humanY + (humanSize / 2) >= windowHeight) {
+		humanY = windowHeight - (humanSize / 2);
+		humanV *= humanDamping;
+		humanSize *= 0.8;
 	}
 
-	if (zombieA >= endPosition && zombieAspeed >= 0) {
-		zombieAspeed = random(-3, -6);
-		zombieAcolor(random(50, 255), random(50, 255), random(50, 255));
-		zombieAsize = random(30, 200);
-	} 
+	fill(zombieColor);
+	stroke(240, 50, 50);
+	strokeWeight(20);
+	ellipse(windowWidth / 4, zombieY, zombieSize, zombieSize);
+	zombieY += zombieV;
+	zombieV += zombieA;
+
+	if (zombieY + (zombieSize / 2) >= windowHeight) {
+		zombieY = windowHeight - (zombieSize / 2);
+		zombieV *= zombieDamping;
+		zombieSize *= 0.5;
+	}
+
+
 
 }
+//Nathan Pierce
+//Leo 
